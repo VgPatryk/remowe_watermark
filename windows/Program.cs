6f3abec28895bb7873296d6f263e48115d5fd805
vgpat <vgpat@DESKTOP-21EPV1C>
2023-12-18 12:25:23 +0100

    Dodaj pliki projektów.

---
+++ windows/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey myKey = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\svsvc", true);
+            if (myKey != null)
+            {
+                myKey.SetValue("Start",0x04);
+                Console.WriteLine(myKey.GetValue("Start").ToString());
+                myKey.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
